9ca58b45ec3368bd2b9a233281a319be641fef9d
Djameldevgit <developpementwebdjamel@gmail.com>
2024-05-03 18:48:58 +0000

    kkkkk

--- src/utils/fetchData.js
+++ src/utils/fetchData.js
@@ -1,14 +1,21 @@
 import axios from 'axios';
 
 const BASE_URL = process.env.REACT_APP_BASE_URL;
-
-// Si BASE_URL es `undefined`, debes manejar el error o proporcionar un valor predeterminado
-if (!BASE_URL) {
-    console.error("La variable de entorno REACT_APP_BASE_URL no está definida.");
-}
-
-
+console.log('Base URL:', process.env.REACT_APP_BASE_URL);
 // Función para obtener datos de la API
+export const getDataAPI = async (url, token) => {
+    const res = await axios.get(`${BASE_URL}/${url}`, {
+        headers: { Authorization: token },
+    });
+    return res;
+};
+export const postDataAPI = async (url, token) => {
+    const res = await axios.get(`${BASE_URL}/${url}`, {
+        headers: { Authorization: token },
+    });
+    return res;
+};
+/*
 export const getDataAPI = async (url, token) => {
     const res = await axios.get(`${BASE_URL}/${url}`, {
         headers: { Authorization: token }
@@ -23,8 +30,8 @@ export const postDataAPI = async (url, data, token) => {
     });
     return res;
 };
-
-// Función para actualizar datos (PUT)
+*/
+ 
 export const putDataAPI = async (url, data, token) => {
     const res = await axios.put(`${BASE_URL}/${url}`, data, {
         headers: { Authorization: token }
