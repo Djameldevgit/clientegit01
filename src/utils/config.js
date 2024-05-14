08f2ed9de6abddddeb0ef764f346ab8f9c3c2dbd
Djameldevgit <developpementwebdjamel@gmail.com>
2024-05-14 17:09:49 +0000

    ooooo

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,2 +1,4 @@
-export const API_URL = "https://servidorrender.onrender.com"
- 
+// config.js
+const SERVER_URL = process.env.REACT_APP_API_URL || 'http://localhost:5000';
+
+export default SERVER_URL;
