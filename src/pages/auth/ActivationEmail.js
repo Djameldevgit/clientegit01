92073930ac01351e9b3f77bf7b0164924137af40
Djameldevgit <developpementwebdjamel@gmail.com>
2024-05-14 21:31:23 +0000

    oooo

--- src/pages/auth/ActivationEmail.js
+++ src/pages/auth/ActivationEmail.js
@@ -1,24 +1,23 @@
 import React, { useState, useEffect } from 'react'
 import { useParams } from 'react-router-dom'
-import axios from 'axios'
+ 
 import { showErrMsg, showSuccessMsg } from '../../utils/notification/Notification'
 import { Link } from 'react-router-dom';
-
+ 
 import Grid from '@mui/material/Grid';
+import { postDataAPI } from '../../utils/fetchData';
  
 function ActivationEmail() {
     const { activation_token } = useParams()
     const [err, setErr] = useState('')
     const [success, setSuccess] = useState('')
-
+ 
     useEffect(() => {
      
         const activationEmail = async () => {
             try {
-                const res = await axios.post('/api/activation', { activation_token }, {
-                    headers: {
-                        'Content-Type': 'application/json'
-                    }
+                const res = await postDataAPI('activation', { activation_token }, {
+                  
                 })
         
                 setSuccess(res.data.msg)
